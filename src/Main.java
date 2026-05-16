6c8702b2c0c7ce4af7e3d10c498cfa6951b04c1d
i4ego <me@i4ego.ru>
2026-05-16 23:36:48 +0500

    large update

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,38 @@ import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        String[] blur = new String[]{"██", "▓▓", "▒▒", "░░"};
-        Vector<Vector<double[]>> chunk = MapGenerator.generateChunk(8);
-        System.out.println(MapGenerator.export(chunk, blur));
+        if (args.length < 1) {
+            help();
+        }
+        else if (args[0]=="help") {help();}
+        else if (args.length == 1) {
+            try {
+                int size = Integer.parseInt(args[0]);
+                Vector<Vector<double[]>> chunk = MapGenerator.generateChunk(size);
+                System.out.println(MapGenerator.export(chunk, MapGenerator.blockblur));
+                System.exit(0);
+            }
+            catch (Exception e) {
+                System.out.println("Invalid arguments!");
+                System.exit(1);
+            }
+        }
+        else {
+            try {
+                int sizeX = Integer.parseInt(args[0]);
+                int sizeY = Integer.parseInt(args[1]);
+                Vector<Vector<double[]>> chunk = MapGenerator.generateChunk(sizeX, sizeY);
+                System.out.println(MapGenerator.export(chunk, MapGenerator.blockblur));
+                System.exit(0);
+            }
+            catch (Exception e) {
+                System.out.println("Invalid arguments!");
+                System.exit(1);
+            }
+        }
+        System.exit(0);
+    }
+    public static void help() {
+        System.out.println("Usage:\n   .. <x> <y>\n   .. <size>\nExamples:\n   .. 8 16\n   .. 16");
     }
 }
--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -2,7 +2,10 @@ import java.util.Random;
 import java.util.Vector;
 
 public class MapGenerator {
-    private static Vector<double[]> generateMatrix (int maxSize) {
+    public static String[] blockblur = new String[]{"██", "▓▓", "▒▒", "░░"};
+    public static String[] asciiblur = new String[]{"@@", "&&", "||", ".."};
+
+    public static Vector<double[]> generateMatrix (int maxSize) {
         Vector<double[]> matrix = new Vector<>(maxSize);
 
         Random r = new Random();
@@ -16,6 +19,7 @@ public class MapGenerator {
         }
         return matrix;
     }
+
     public static Vector<Vector<double[]>> generateChunk(int maxSize) {
 
         Vector<Vector<double[]>> chunk = new Vector<>(maxSize){};
@@ -24,10 +28,20 @@ public class MapGenerator {
         }
         return chunk;
     }
+
+    public static Vector<Vector<double[]>> generateChunk(int x, int y) {
+
+        Vector<Vector<double[]>> chunk = new Vector<>(x){};
+        for (int i = 0; i < x; i++) {
+            chunk.add(generateMatrix(y));
+        }
+        return chunk;
+    }
+
     public static String export(Vector<Vector<double[]>> matrix, String[] blur){
         StringBuilder toExport = new StringBuilder();
         for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix.size(); j++) {
+            for (int j = 0; j < matrix.get(i).size(); j++) {
                 int id = (int) (matrix.get(i).get(j)[0] * blur.length);
                 toExport.append(blur[id]);
             }
